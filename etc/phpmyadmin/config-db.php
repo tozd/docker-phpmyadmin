36fb7c43d9ea0d5e5f768e67dcb602980cce2465
Mitar <mitar.git@tnode.com>
2014-07-07 01:00:49 -0700

    Initial files for phpMyAdmin Docker image.

---
+++ etc/phpmyadmin/config-db.php
@@ -0,0 +1,19 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+## Mon, 07 Jul 2014 00:01:25 -0700
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+$dbuser='';
+$dbpass='';
+$basepath='';
+$dbname='';
+$dbserver='dbmysql.mysql.server2.docker';
+$dbport='3306';
+$dbtype='mysql';
---
+++ run.sh
@@ -0,0 +1,7 @@
+#/bin/bash -e
+
+mkdir -p /srv/var/log/phpmyadmin
+
+# TODO: We should not trust whole Docker IP range
+
+docker run -d --name phpmyadmin -h phpmyadmin.phpmyadmin.server2.docker -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.net -e REMOTES=mail.cloyne.net -v /srv/var/log/phpmyadmin:/var/log/nginx cloyne/phpmyadmin
