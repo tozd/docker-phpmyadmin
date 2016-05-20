fa57fe97a3f0e88c7e21335de857afc3f248b724
Mitar <mitar.git@tnode.com>
2016-05-20 00:19:14 -0700

    Use dockergen to serve HTTP.

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 mkdir -p /srv/var/log/phpmyadmin
 
-# TODO: We should not trust whole Docker IP range
+# TODO: We should not trust whole Docker IP range.
 
-docker run -d --restart=always --name phpmyadmin --hostname phpmyadmin -e SET_REAL_IP_FROM=172.17.0.0/16 -e ADMINADDR=root@cloyne.org -e REMOTES=mail.cloyne.net -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/phpmyadmin:/var/log/nginx cloyne/phpmyadmin
+docker run --detach=true --restart=always --name phpmyadmin --hostname phpmyadmin \
+ --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/phpmyadmin/ \
+ --env SET_REAL_IP_FROM=172.17.0.0/16 --env ADMINADDR=root@cloyne.org --env REMOTES=mail.cloyne.net \
+ --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/phpmyadmin:/var/log/nginx \
+ cloyne/phpmyadmin
