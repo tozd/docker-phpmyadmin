b3aefff3c84db4973434560f3db09e0ee71255b2
Mitar <mitar.git@tnode.com>
2017-05-16 17:07:16 -0700

    Updating run.sh.

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ mkdir -p /srv/var/log/phpmyadmin
 docker run --detach=true --restart=always --name phpmyadmin --hostname phpmyadmin \
  --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/phpmyadmin/ \
  --env SET_REAL_IP_FROM=172.17.0.0/16 --env ADMINADDR=root@cloyne.org --env REMOTES=mail.cloyne.net \
- --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/phpmyadmin:/var/log/nginx \
- cloyne/phpmyadmin
+ --volume /srv/var/log/phpmyadmin:/var/log/nginx \
+ tozd/phpmyadmin
